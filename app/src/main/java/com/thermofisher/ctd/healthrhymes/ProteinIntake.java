5fe71d3c38a504e43c71066b2a5eca4641a7e05a
Prasanna Venkatesalu <Prasanna.Venkatesalu@thermofisher.com>
2018-03-28 13:13:35 +0530

    Included ProteinIntake activity and added code to navigate to different activity page

---
+++ app/src/main/java/com/thermofisher/ctd/healthrhymes/ProteinIntake.java
@@ -0,0 +1,41 @@
+package com.thermofisher.ctd.healthrhymes;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class ProteinIntake extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_protein_intake);
+
+        btnNext = ((Button) findViewById(R.id.nextbutton));
+        btnNext.setOnClickListener(ProteinIntake.this);
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        //do something...
+    }
+
+}
