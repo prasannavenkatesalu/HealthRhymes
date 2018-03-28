33cc758fa5fe5dfbf1aaa179544eaa5324aef21b
Prasanna Venkatesalu <Prasanna.Venkatesalu@thermofisher.com>
2018-03-28 11:33:07 +0530

    Initial commit

---
+++ app/src/main/java/com/thermofisher/ctd/healthrhymes/HealthInformation.java
@@ -0,0 +1,14 @@
+package com.thermofisher.ctd.healthrhymes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HealthInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.information_health);
+        System.out.println("Code Ends");
+    }
+}
