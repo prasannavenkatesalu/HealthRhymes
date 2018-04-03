1a2ac7fc9db8a2dedd31749cd3dcd48c0dd363d6
Prasanna Venkatesalu <Prasanna.Venkatesalu@thermofisher.com>
2018-04-03 10:13:26 +0530

    enabled scrolling for listview controls in HealthInformation

--- app/src/main/java/com/thermofisher/ctd/healthrhymes/HealthInformation.java
+++ app/src/main/java/com/thermofisher/ctd/healthrhymes/HealthInformation.java
@@ -4,6 +4,7 @@ package com.thermofisher.ctd.healthrhymes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -32,18 +33,81 @@ public class HealthInformation extends AppCompatActivity {
         knownDiseaseslistItems = getResources().getStringArray(R.array.knownDiseasesArr);
         ArrayAdapter<String> knownDiseaseAdapter = new ArrayAdapter<String>(this,R.layout.multipleitemselection,R.id.txt_mul_lst,knownDiseaseslistItems);
         knownDiseaseList.setAdapter(knownDiseaseAdapter);
+        knownDiseaseList.setOnTouchListener(new ListView.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Disallow ScrollView to intercept touch events.
+                        v.getParent().requestDisallowInterceptTouchEvent(true);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        // Allow ScrollView to intercept touch events.
+                        v.getParent().requestDisallowInterceptTouchEvent(false);
+                        break;
+                }
+
+                // Handle ListView touch events.
+                v.onTouchEvent(event);
+                return true;
+            }
+        });
 
         familyKnownList = (ListView)findViewById(R.id.familyknownList);
         familyKnownList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         knownDiseaseslistItems = getResources().getStringArray(R.array.familyKnownDiseasesArr);
         ArrayAdapter<String> familyKnownAdapter = new ArrayAdapter<String>(this,R.layout.multipleitemselection,R.id.txt_mul_lst,knownDiseaseslistItems);
         familyKnownList.setAdapter(familyKnownAdapter);
+        familyKnownList.setOnTouchListener(new ListView.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Disallow ScrollView to intercept touch events.
+                        v.getParent().requestDisallowInterceptTouchEvent(true);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        // Allow ScrollView to intercept touch events.
+                        v.getParent().requestDisallowInterceptTouchEvent(false);
+                        break;
+                }
+
+                // Handle ListView touch events.
+                v.onTouchEvent(event);
+                return true;
+            }
+        });
 
         knownDiseaseList = (ListView)findViewById(R.id.deficiencyVitamins);
         knownDiseaseList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         knownDiseaseslistItems = getResources().getStringArray(R.array.vitaminDefArr);
         ArrayAdapter<String> deficiencyVitaminAdapter = new ArrayAdapter<String>(this,R.layout.multipleitemselection,R.id.txt_mul_lst,knownDiseaseslistItems);
         knownDiseaseList.setAdapter(deficiencyVitaminAdapter);
+        knownDiseaseList.setOnTouchListener(new ListView.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Disallow ScrollView to intercept touch events.
+                        v.getParent().requestDisallowInterceptTouchEvent(true);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        // Allow ScrollView to intercept touch events.
+                        v.getParent().requestDisallowInterceptTouchEvent(false);
+                        break;
+                }
+
+                // Handle ListView touch events.
+                v.onTouchEvent(event);
+                return true;
+            }
+        });
     }
 
 
